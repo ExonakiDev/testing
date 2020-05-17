bbcb196e59c3abf309e1b328ec6a063bec4cde6e
ExonakiDev <rsanyal17@yahoo.co.in>
2020-05-17 14:49:15 +0530

    Breast Cancer RF Commit

--- testing.R
+++ testing.R
@@ -1,2 +1,27 @@
-print("This file was created within RStudio") 
-print("And now it lives on GitHub")
+library(randomForest)
+require(caTools)
+library(caret)
+
+bcdata<-read.csv("bcdata.csv")
+dim(bcdata)
+names(bcdata)
+
+summary(bcdata)
+sapply(bcdata,class)
+bcdata<- transform(bcdata,diagnosis=as.factor(diagnosis))
+colSums(is.na(bcdata))
+
+sample= sample.split(bcdata$diagnosis, SplitRatio = 0.75)
+train=subset(bcdata, sample==TRUE)
+test= subset(bcdata, sample==FALSE)
+
+rf<-randomForest(diagnosis~.,data=train, mtry=4,ntree=500, importance=TRUE)
+rf
+varImpPlot(rf)
+
+pred<- predict(rf, train, type="class")
+table(pred, train$diagnosis)
+
+validation<- predict(rf, test, type="class")
+mean(validation==test$diagnosis)
+table(validation, test$diagnosis)
